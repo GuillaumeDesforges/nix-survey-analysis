7a0b8067d558f526ad633d5ff6289260b4362e38
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-08-24 14:47:35 +0200

    Print table of questions

--- scripts/questions.py
+++ scripts/questions.py
@@ -7,16 +7,28 @@ from nixos.survey_analysis.questions import (
 )
 
 df = pl.read_csv("./data/results-survey2023.csv")
-questions_by_id = group_by_question_id(parse_questions(df))
 questions_by_id_by_type = group_by_question_type(
-    questions_by_id=questions_by_id,
+    questions_by_id=group_by_question_id(parse_questions(df)),
     df=df,
 )
 
-for question_type, questions_by_id in questions_by_id_by_type.__dict__.items():
-    print(f"question_type={question_type}")
-    for question_id, questions in questions_by_id.items():
-        print(f"question_id={question_id}")
-        for question in questions:
-            print(question)
-    print()
+df_questions = pl.DataFrame(
+    [
+        {
+            "question_type": question_type,
+            "question_id": question_id,
+            "question_text": questions[0].question_text,
+            "count_unique_answers": len(
+                set(
+                    answer
+                    for question in questions
+                    for answer in df[question.column].drop_nulls().to_list()
+                )
+            ),
+        }
+        for question_type, questions_by_id in questions_by_id_by_type.__dict__.items()
+        for question_id, questions in questions_by_id.items()
+    ]
+)
+
+print(df_questions.to_pandas().to_markdown())
